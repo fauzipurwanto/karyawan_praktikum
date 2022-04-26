7300f8b408589c17d01fce2a1fe4afa2a3abd528
Muhammad Fauzi Purwanto <fauzipurwanto7787@gmail.com>
2022-04-26 13:47:44 +0800

    Menambahkan Card Pada dashboard.php

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -5,6 +5,26 @@
         </div>
       </div> 
 
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <div class="card bg-primary" style="height:9rem">
+              </div>
+            </div>
+            <div class="col">
+            <div class="card bg-success" style="height:9rem">
+              </div>
+            </div>
+            <div class="col">
+            <div class="card bg-warning" style="height:9rem">
+              </div>
+            </div>
+            <div class="col">
+            <div class="card bg-danger" style="height:9rem">
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   
