a1b37167680a8edd9b666efd30a3a8f1d356e4f5
Muhammad Fauzi Purwanto <fauzipurwanto7787@gmail.com>
2022-05-18 11:45:58 +0800

    Validasi Data yang kosong

--- pages/karyawancreate.php
+++ pages/karyawancreate.php
@@ -31,17 +31,23 @@
 
             ?>
 
-                 <label for="nama_karyawan" class="form-label">Nama Karyawan</label>
-                 <input type="text" class="form-control" id="nama_karyawan" name="nama_karyawan">
             </div>
-
+        </div>
+        <div>
+            <form action="" method="post">
+            <div class="mb-3">
+                <label for="nik" class="form-label">Nomor Induk Karyawan</label>
+                <input type="text" class="form-control" id="nik" name="nik" required>
+            </div>
+            <div class="mb-3">
+                <label for="nama_karyawan" class="form-label">Nama Karyawan</label>
+                <input type="text" class="form-control" id="nama_karyawan" name="nama_karyawan" required>
+            </div>
             <div>
-            <label for="jenis_kelamin" class="form-label">Jenis Kelamin</label>
-
-        </div>
-        </div>
+                <label for="jenis_kelamin" class="form-label">Jenis Kelamin</label>
+            </div>
             <div class="form-check">
-                <input class="form-check-input" type="radio" value="Laki-laki" name="jenis_kelamin" id="jenis_kelamin1">
+                <input class="form-check-input" type="radio" value="Laki-laki" name="jenis_kelamin" id="jenis_kelamin1" required>
                 <label class="form-check-label" for="jenis_kelamin1">
                     Laki-laki
                 </label>
@@ -51,12 +57,11 @@
                 <label class="form-check-label" for="jenis_kelamin2">
                     Perempuan
                 </label>
-                
             </div>
             <div class="mb-3">
                 <label for="status_menikah" class="form-label">Status Menikah</label>
-                <select class="form-select" aria-label="Default select example" name="status_menikah">
-                    <option selected>Pilih Status</option>
+                <select class="form-select" aria-label="Default select example" name="status_menikah" required>
+                    <option value="" selected>Pilih Status</option>
                     <option value="Belum Menikah">Belum Menikah</option>
                     <option value="Menikah Anak 0">Menikah Anak 0</option>
                     <option value="Menikah Anak 1">Menikah Anak 1</option>
@@ -66,4 +71,4 @@
             <button class="btn btn-success" type="submit" name="button_simpan"><span data-feather="database"></span> Simpan</button>
         </form>
     </div>
-</main> 
+</main>
